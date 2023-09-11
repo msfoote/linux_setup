94493b607d7c04267854e873114ca01d6553ca5f
Matthew Foote <matthew.foote@gmail.com>
2023-09-11 11:45:51 -0700

    Finish

--- setup.sh
+++ setup.sh
@@ -8,8 +8,19 @@ echo "You entered the IP address: $ip_address"
 
 # You can now use the $ip_address variable for further processing
 match=$(ip link  | grep -oP '\b(e\S+):')
-echo "Match found: $match"
 
+# Copy the template and replace the appropriate values
 cp 'templates/00-installer-config.yaml.jinja' '00-installer-config.yaml'
 sed -i "s/{{ network_device_name }}/$match/g" 00-installer-config.yaml
 sed -i "s/{{ ip_address }}/$ip_address/g" 00-installer-config.yaml
+
+# Confirm the ip address is correct
+read -p "Please confirm you wish to use $ip_address? (Y/N): " response
+if [[ "$response" =~ ^[Yy] ]]; then
+    sudo cp '/etc/netplan/00-installer-config.yaml' '/etc/netplan/00-installer-config.yaml.bak'
+    sudo cp '00-installer-config.yaml' '/etc/netplan/00-installer-config.yaml'
+    sudo netplan apply
+    echo "Complete."
+else
+    echo "Aborted."
+fi

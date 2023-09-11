23bce3d39d7bb9f2cb3006389a20300bf14388a3
Matthew Foote <matthew.foote@gmail.com>
2023-09-11 11:36:41 -0700

    Fix

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ echo "You entered the IP address: $ip_address"
 match=$(ip link  | grep -oP '\b(e\S+):')
 echo "Match found: $match"
 
-cp 'templates/00-installer-config.yaml.jinja' '~/00-installer-config.yaml'
+cp 'templates/00-installer-config.yaml.jinja' '00-installer-config.yaml'
 sed -i "s/{{ network_device_name }}/$match/g" 00-installer-config.yaml
 sed -i "s/{{ ip_address }}/$ip_address/g" 00-installer-config.yaml

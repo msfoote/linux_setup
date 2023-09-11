081f227019fa301a9938b99b8f194c251c2ef0de
Matthew Foote <matthew.foote@gmail.com>
2023-09-11 11:54:03 -0700

    Fix code

--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ match=$(ip link  | grep -oP '\b(e\S+):')
 
 # Confirm the ip address is correct
 read -p "Please confirm you wish to use $ip_address? (Y/N): " response
-if [ "$response" =~ ^[Yy] ]; then
+response_lower=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [ "$response_lower" ==  y*]; then
     # Copy the template and replace the appropriate values
     cp 'templates/00-installer-config.yaml.jinja' '00-installer-config.yaml'
     sed -i "s/{{ network_device_name }}/$match/g" 00-installer-config.yaml
